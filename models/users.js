ae11bb76177b7121d0aaf0b02293a2632a6623ac
niriksha02 <nirikshashree02@gmail.com>
2020-07-15 04:06:44 -0100

    passport

--- models/users.js
+++ models/users.js
@@ -1,23 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var User = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
+const User = new Schema({
   admin: {
     type: Boolean,
     default: false,
   },
 });
 
+User.plugin(passportLocalMongoose);
+
 module.exports = mongoose.model(
   'User',
   User
-); 
+);
